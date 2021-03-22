5c4c02b93949d17ec637af085c90f89bbb29054d
jakovnovak30 <80671098+jakovnovak30@users.noreply.github.com>
2021-03-22 17:32:17 +0100

    Add files via upload

--- lopta.py
+++ lopta.py
@@ -5,18 +5,18 @@ class Lopta:
     def __init__(self, tocka):
         self.k = tocka
 
-        smjer1 = random.randrange(0,1)
-        smjer2 = random.randrange(0,1)
+        smjer1 = random.randrange(0,2)
+        smjer2 = random.randrange(0,2)
 
         if smjer1 == 0:
-            self.brzinah = random.randrange(-8, -4)
+            self.brzinah = random.randrange(-8, -5)
         else:
-            self.brzinah = random.randrange(4, 8)
+            self.brzinah = random.randrange(5, 8)
 
         if smjer2 == 0:
-            self.brzinav = random.randrange(-8, -4)
+            self.brzinav = random.randrange(-4, -2)
         else:
-            self.brzinav = random.randrange(4, 8)    
+            self.brzinav = random.randrange(2, 4)
 
         self.zadnji = 0 #ko je zadnji diral loptu
 
@@ -32,7 +32,7 @@ class Lopta:
         y = y + self.brzinav
         x = x + round(self.brzinah*1.5)
 
-        if x < 0 or x > 700 or y < 45 or y > 500:
+        if x < 45 or x > 700 or y < 45 or y > 455:
             return self.zadnji
 
         self.k = (x,y)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import igrac
 import lopta
 import random
+import math
 
 pygame.init()
 ekran = pygame.display.set_mode((700, 500))
@@ -13,7 +14,7 @@ def igra():
     skor2 = 0
     igrac1 = igrac.Plejer((50,200), (50,300))
     igrac2 = igrac.Plejer((650,200), (650,300))
-    lopta1 = lopta.Lopta((350,250))
+    lopta1 = lopta.Lopta((350,300))
 
     igrac1.nacrtaj(ekran)
     igrac2.nacrtaj(ekran)
@@ -50,27 +51,30 @@ def igra():
             if igrac1.t1[1] <= lopta1.k[1] and igrac1.t2[1] >= lopta1.k[1]:
                 lopta1.zadnji = 1
 
-                if abs(lopta1.k[1] - igrac1.t1[1]) < abs(lopta1.k[1] - igrac1.t2[1]):
-                    lopta1.brzinah *= -1*round(random.randrange(7, 13)/10)
-                    lopta1.brzinav = round(abs(lopta1.brzinav * random.randrange(7, 13)/10))
+                odsredine = min(abs(lopta1.k[1] - igrac1.t2[1]), abs(lopta1.k[1] - igrac1.t1[1]))
+                if odsredine != 0: kut = math.atan(8/odsredine)
                 else:
-                    lopta1.brzinah *= -1*round(random.randrange(7, 13)/10)
-                    lopta1.brzinav = -1*round(abs(lopta1.brzinav * random.randrange(7, 13)/10))
+                    kut = math.pi
+                brzina = math.sqrt(pow(lopta1.brzinah, 2) + pow(lopta1.brzinav, 2))
 
-                lopta1.brzina = random.randrange(3,7)
+                lopta1.brzinah = brzina*math.cos(kut)
+
+                lopta1.brzina = brzina*math.sin(kut)
 
         elif igrac2.t1[0] <= lopta1.k[0] + 13 and (lopta1.zadnji == 1 or lopta1.zadnji == 0):
             if igrac2.t1[1] <= lopta1.k[1] and igrac2.t2[1] >= lopta1.k[1]:
                 lopta1.zadnji = 2
 
-                if abs(lopta1.k[1] - igrac2.t1[1]) < abs(lopta1.k[1] - igrac2.t2[1]):
-                    lopta1.brzinah *= -1*round(random.randrange(7, 13)/10)
-                    lopta1.brzinav = abs(lopta1.brzinav * round(random.randrange(7, 13)/10))
+                odsredine = min(abs(lopta1.k[1] - igrac2.t2[1]), abs(lopta1.k[1] - igrac2.t1[1]))
+                if odsredine != 0: kut = math.atan(8/odsredine)
                 else:
-                    lopta1.brzinah *= -1*round(random.randrange(7, 13)/10)
-                    lopta1.brzinav = -1*abs(lopta1.brzinav * round(random.randrange(7, 13)/10))
+                    kut = math.pi
 
-                lopta1.brzina = random.randrange(3,7)
+                brzina = math.sqrt(pow(lopta1.brzinah, 2) + pow(lopta1.brzinav, 2))
+
+                lopta1.brzinah = -1*brzina*math.cos(kut)
+
+                lopta1.brzinav = -1*brzina*math.sin(kut)
 
         promjena = lopta1.update(ekran)
 
@@ -79,7 +83,7 @@ def igra():
                 skor1 = skor1 + 1
             elif promjena == 2:
                 skor2 = skor2 + 1
-            lopta1 = lopta1 = lopta.Lopta((350,250))
+            lopta1 = lopta1 = lopta.Lopta((350,300))
 
         lopta1.nacrtaj(ekran)
         igrac1.nacrtaj(ekran)
